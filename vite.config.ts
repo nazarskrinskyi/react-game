a1065bc0333396bd38d5e85f9a3082743f7092ad
Nazar Skrinskyi <nazar.skrinskyi@drumncode.com>
2025-05-03 22:41:25 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import remixPlugin from "vite-plugin-remix";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    remixPlugin()
+  ],
   css: {
     postcss: './postcss.config.js', // Ensure PostCSS is referenced correctly
   },
